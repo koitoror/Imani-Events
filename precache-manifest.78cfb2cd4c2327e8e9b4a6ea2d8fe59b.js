0c4e9e2ea0044eaeb023542d715a8ff040921d5b
Daniel Kamar <kamarster@gmail.com>
2020-03-22 23:08:20 +0300

    Updates

---
+++ precache-manifest.78cfb2cd4c2327e8e9b4a6ea2d8fe59b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2102629cf9ceee4733c5d2ff658366",
+    "url": "/Imani-Events/index.html"
+  },
+  {
+    "revision": "79b591a80c5496a8cc0a",
+    "url": "/Imani-Events/static/css/2.ba822a8a.chunk.css"
+  },
+  {
+    "revision": "83bbdad0a271388f9686",
+    "url": "/Imani-Events/static/css/main.f0b4e07f.chunk.css"
+  },
+  {
+    "revision": "79b591a80c5496a8cc0a",
+    "url": "/Imani-Events/static/js/2.902a6a52.chunk.js"
+  },
+  {
+    "revision": "03ad7a539ac78b14d19a6a6a77322b49",
+    "url": "/Imani-Events/static/js/2.902a6a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bbdad0a271388f9686",
+    "url": "/Imani-Events/static/js/main.2031eec7.chunk.js"
+  },
+  {
+    "revision": "829bcaa80b843d47c809",
+    "url": "/Imani-Events/static/js/runtime-main.d852c85d.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Imani-Events/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Imani-Events/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Imani-Events/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Imani-Events/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Imani-Events/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Imani-Events/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Imani-Events/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Imani-Events/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Imani-Events/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Imani-Events/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Imani-Events/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Imani-Events/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Imani-Events/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Imani-Events/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Imani-Events/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Imani-Events/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
